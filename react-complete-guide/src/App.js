139108807dd7acf26b1fd427ad73be0220c6c1aa
Castiel <castiel.andrew@gmail.com>
2018-12-26 20:41:19 -0800

    First Charpter done

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -31,10 +31,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroudColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor:'pointer'
+    };
+    
     return (
       <div className="App">
         <h1>Hi its me</h1>
-        <button onClick={() => this.switchNameHandler('Maxminue')}>Switch Name</button>
+        <button 
+          style= {style}
+          onClick={() => this.switchNameHandler('Maxminue')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age = {this.state.persons[0].age}>My Hobbie: Racing</Person>
